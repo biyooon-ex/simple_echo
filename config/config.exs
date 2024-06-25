8ebd938d4b11421e81f44fdea5b4b92855d8c824
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2024-06-25 19:24:29 +0900

    set logger level

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,11 @@
 import Config
 
+# Compile Zenohex NIF module locally
+# TODO: delete after next version of Zenohex has been released
 config :rustler_precompiled, :force_build, zenohex: true
+
+# Configures Elixir's Logger
+config :logger, :console,
+  level: :debug,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
