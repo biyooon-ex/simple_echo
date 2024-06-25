d8210cf34ed58767cfb08aedd6c4babaea4afcd8
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2024-06-25 13:50:01 +0900

    able to set ping/pong_key by env_ver

--- lib/simple_echo/zenohex_pingpong.ex
+++ lib/simple_echo/zenohex_pingpong.ex
@@ -2,8 +2,8 @@ defmodule SimpleEcho.ZenohexPingPong do
   use GenServer
   require Logger
 
-  @ping_key "demo/zcam/ping"
-  @pong_key "demo/zcam/pong"
+  @default_ping_key "demo/zcam/ping"
+  @default_pong_key "demo/zcam/pong"
 
   def start_link(_args) do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
@@ -12,10 +12,12 @@ defmodule SimpleEcho.ZenohexPingPong do
   def init(_args) do
     # Open Zenoh session and declare subscriber
     {:ok, session} = Zenohex.open()
-    {:ok, subscriber} = Zenohex.Session.declare_subscriber(session, @ping_key)
+    ping_key = System.get_env("PING_KEY", @default_ping_key)
+    {:ok, subscriber} = Zenohex.Session.declare_subscriber(session, ping_key)
 
     # Declare publisher with created Zenoh session
-    {:ok, publisher} = Zenohex.Session.declare_publisher(session, @pong_key)
+    pong_key = System.get_env("PONG_KEY", @default_pong_key)
+    {:ok, publisher} = Zenohex.Session.declare_publisher(session, pong_key)
 
     state = %{subscriber: subscriber, publisher: publisher}
     recv_timeout(state)
